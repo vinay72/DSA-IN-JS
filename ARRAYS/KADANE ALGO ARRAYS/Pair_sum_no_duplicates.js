e3d614f57b183ad67a37b74e9137df349c30198b
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2024-10-21 17:24:52 +0530

    Update Pair_sum_no_duplicates.js

--- ARRAYS/KADANE ALGO ARRAYS/Pair_sum_no_duplicates.js
+++ ARRAYS/KADANE ALGO ARRAYS/Pair_sum_no_duplicates.js
@@ -1,3 +1,16 @@
+// ALGORITHM
+// Create two pointers i and j, initialize i=0 and j=arr.length - 1
+// Calculate the sum of arr[i] and arr[j]
+// if arr[i] + arr[j] > sum
+//    // we need a smaller value
+//     j--
+// else if   arr[i] + arr[j] < sum
+//    // we need a greater value
+//     i++
+// else
+// i++
+// j--
+
 function pairSum(arr, target) {
     let i = 0
     let j = arr.length - 1
